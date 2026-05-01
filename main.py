d31126056a82f2cd17cc07c1f3e2b56bdc88b461
re-21-12 <victora.caas@gmail.com>
2026-05-01 16:45:07 -0600

    added cruds

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ if __name__ == "__main__":
 
     def abrir_editor(configuracion):
         estado["config"].hide()
-        estado["editor"] = VentanaEditarGrafo('src/data.csv', configuracion=configuracion)
+        estado["editor"] = VentanaEditarGrafo('src/data_case_1.csv', configuracion=configuracion)
         estado["editor"].show()
 
     estado["config"] = VentanaConfiguracion(on_continue=abrir_editor)
